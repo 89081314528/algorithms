4a0ca58494c7bcfea85dbbb626bf4a1bbb1a97eb
julia <89081314528@mail.ru>
2021-04-21 18:10:22 +0300

    20/04

---
+++ src/main/java/ru/julia/Main.java
@@ -0,0 +1,15 @@
+package ru.julia;
+
+/**
+ * 1.1 Определение четности числа
+ */
+public class Main {
+    public static void main(String[] args) {
+        int a = 6;
+        if (a % 2 == 0) {
+            System.out.println("Число четное");
+        } else {
+            System.out.println("Число нечетное");
+        }
+    }
+}
