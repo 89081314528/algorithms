271a1042f8b650d6ff73e06d9eed9b3110ece4c7
julia <89081314528@mail.ru>
2021-04-29 00:55:37 +0300

    28/04 3.3.5 2

--- src/main/java/ru/julia/FastSort3_3_5.java
+++ src/main/java/ru/julia/FastSort3_3_5.java
@@ -10,12 +10,11 @@ import java.util.List;
 public class FastSort3_3_5 {
     public static void main(String[] args) {
         List<Integer> numbers = new ArrayList<>();
-        for (int i = 0; i < 32; i++) {
+        for (int i = 0; i < 16; i++) {
             int number = (int) (Math.random() * 100 - 0);
             numbers.add(number);
         }
         System.out.println(numbers);
-
 
         List<List<Integer>> listListov = share(numbers);
         System.out.println(listListov);
@@ -35,82 +34,92 @@ public class FastSort3_3_5 {
             partSize = listListov.get(0).size();
         }
     }
-public static List<List<Integer>> share (List<Integer> numbers) {
-    while (!isSortedByMiddle(numbers)) {
-        sortByMiddle(numbers);
-    }
+
+    public static List<List<Integer>> share(List<Integer> numbers) {
+        while (!isSortedByMiddle(numbers)) {
+            sortByMiddle(numbers);
+        }
 //    System.out.println("numbers = " + numbers);
-    List<List<Integer>> listListov = new ArrayList<>();
-    List<Integer> list1 = new ArrayList<>();
-    List<Integer> list2 = new ArrayList<>();
-    for (int i = 0; i < numbers.size()/2; i++) {
-        list1.add(numbers.get(i));
-    }
+        List<List<Integer>> listListov = new ArrayList<>();
+        List<Integer> list1 = new ArrayList<>();
+        List<Integer> list2 = new ArrayList<>();
+        for (int i = 0; i < numbers.size() / 2; i++) {
+            list1.add(numbers.get(i));
+        }
 //    while (!isSortedByMiddle(list1)) {
 //        sortByMiddle(list1);
 //    }
-    for (int i = numbers.size()/2; i < numbers.size(); i++) {
-        list2.add(numbers.get(i));
-    }
+        for (int i = numbers.size() / 2; i < numbers.size(); i++) {
+            list2.add(numbers.get(i));
+        }
 //    while (!isSortedByMiddle(list2)) {
 //        sortByMiddle(list2);
 //    }
 //    System.out.println(list2);
-    listListov.add(list1);
-    listListov.add(list2);
-    return listListov;
-}
+        listListov.add(list1);
+        listListov.add(list2);
+        return listListov;
+    }
+
     public static List<Integer> sortByMiddle(List<Integer> numbers) {
-            int middle = numbers.get(numbers.size() / 2 - 1);
-//    System.out.println("middle " + middle);
-            int maxIndex = 0;
-            int max = numbers.get(0);
-            for (int j = 0; j < numbers.size(); j++) {
+        int middle = numbers.get(numbers.size() / 2 - 1);
+        int middleIndex = numbers.size() / 2 - 1;
+        System.out.println("middle " + middle);
+        int maxIndex = 0;
+        int max = numbers.get(0);
+        for (int j = 0; j < numbers.size(); j++) {
+            if (numbers.get(j) >= middle) {
+                max = numbers.get(j);
+            System.out.println(max);
+                maxIndex = j;
+                break;
+            }
+        }
+        int min = numbers.get(numbers.size() - 1);
+        int minIndex = numbers.size() - 1;
+        for (int j = numbers.size() - 1; j >= 0; j--) {
+            if (numbers.get(j) <= middle) {
+                min = numbers.get(j);
+                minIndex = j;
+                break;
+            }
+        }
+        System.out.println("min = " + min);
+        System.out.println(minIndex);
+        System.out.println("max = " + max);
+        System.out.println(maxIndex);
+        if (maxIndex < minIndex && max >= middle && min <= middle) {
+            numbers.set(maxIndex, min);
+            numbers.set(minIndex, max);
+            System.out.println("variant 1");
+        }
+
+
+    System.out.println(numbers);
+        while (maxIndex < minIndex) {
+            System.out.println("in");
+            for (int j = maxIndex + 1; j < numbers.size(); j++) {
                 if (numbers.get(j) >= middle) {
                     max = numbers.get(j);
-//            System.out.println(max);
                     maxIndex = j;
                     break;
                 }
             }
-            int min = numbers.get(0);
-            int minIndex = 0;
-            for (int j = numbers.size() - 1; j >= 0; j--) {
+            for (int j = minIndex - 1; j >= 0; j--) {
                 if (numbers.get(j) <= middle) {
                     min = numbers.get(j);
-//            System.out.println(min);
                     minIndex = j;
                     break;
                 }
             }
-            if (maxIndex < minIndex && numbers.get(maxIndex) >= middle && numbers.get(minIndex) <= middle) {
+            if (maxIndex < minIndex && max >= middle && min <= middle) {
                 numbers.set(maxIndex, min);
                 numbers.set(minIndex, max);
+                System.out.println("variant2");
             }
-//    System.out.println(numbers);
-            while (maxIndex < minIndex) {
-                for (int j = maxIndex + 1; j < numbers.size(); j++) {
-                    if (numbers.get(j) >= middle) {
-                        max = numbers.get(j);
-//                System.out.println(max);
-                        maxIndex = j;
-                        break;
-                    }
-                }
-                for (int j = minIndex - 1; j >= 0; j--) {
-                    if (numbers.get(j) <= middle) {
-                        min = numbers.get(j);
-//                System.out.println(min);
-                        minIndex = j;
-                        break;
-                    }
-                }
-                if (maxIndex < minIndex && numbers.get(maxIndex) >= middle && numbers.get(minIndex) <= middle) {
-                    numbers.set(maxIndex, min);
-                    numbers.set(minIndex, max);
-                }
-//        System.out.println(numbers);
-            }
+
+        }
+
         return numbers;
     }
 
