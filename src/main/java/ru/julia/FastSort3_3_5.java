ff2622c2688bd56a6b57b76d894ea42fb04498c8
julia <89081314528@mail.ru>
2021-04-29 14:20:05 +0300

    29/04 3.3.5

--- src/main/java/ru/julia/FastSort3_3_5.java
+++ src/main/java/ru/julia/FastSort3_3_5.java
@@ -10,7 +10,7 @@ import java.util.List;
 public class FastSort3_3_5 {
     public static void main(String[] args) {
         List<Integer> numbers = new ArrayList<>();
-        for (int i = 0; i < 16; i++) {
+        for (int i = 0; i < 50; i++) {
             int number = (int) (Math.random() * 100 - 0);
             numbers.add(number);
         }
@@ -19,7 +19,7 @@ public class FastSort3_3_5 {
         List<List<Integer>> listListov = share(numbers);
         System.out.println(listListov);
         int partSize = listListov.get(0).size();
-        while (partSize > 3) {
+        while (partSize >= 2) {
             int size = listListov.size();
             for (int i = 0; i < size; i++) {
                 List<List<Integer>> listListov1 = share(listListov.get(i));
@@ -46,16 +46,11 @@ public class FastSort3_3_5 {
         for (int i = 0; i < numbers.size() / 2; i++) {
             list1.add(numbers.get(i));
         }
-//    while (!isSortedByMiddle(list1)) {
-//        sortByMiddle(list1);
-//    }
+
         for (int i = numbers.size() / 2; i < numbers.size(); i++) {
             list2.add(numbers.get(i));
         }
-//    while (!isSortedByMiddle(list2)) {
-//        sortByMiddle(list2);
-//    }
-//    System.out.println(list2);
+
         listListov.add(list1);
         listListov.add(list2);
         return listListov;
@@ -64,13 +59,12 @@ public class FastSort3_3_5 {
     public static List<Integer> sortByMiddle(List<Integer> numbers) {
         int middle = numbers.get(numbers.size() / 2 - 1);
         int middleIndex = numbers.size() / 2 - 1;
-        System.out.println("middle " + middle);
+//        System.out.println("middle " + middle);
         int maxIndex = 0;
         int max = numbers.get(0);
         for (int j = 0; j < numbers.size(); j++) {
             if (numbers.get(j) >= middle) {
                 max = numbers.get(j);
-            System.out.println(max);
                 maxIndex = j;
                 break;
             }
@@ -84,20 +78,37 @@ public class FastSort3_3_5 {
                 break;
             }
         }
-        System.out.println("min = " + min);
-        System.out.println(minIndex);
-        System.out.println("max = " + max);
-        System.out.println(maxIndex);
+//        System.out.println("min = " + min);
+//        System.out.println(minIndex);
+//        System.out.println("max = " + max);
+//        System.out.println(maxIndex);
         if (maxIndex < minIndex && max >= middle && min <= middle) {
+            if (min == max && maxIndex == middleIndex) {
+                for (int j = minIndex - 1; j >= 0; j--) {
+                    if (numbers.get(j) < middle) {
+                        min = numbers.get(j);
+                        minIndex = j;
+                        break;
+                    }
+                }
+            }
+            if (min == max && minIndex == middleIndex) {
+                for (int j = maxIndex + 1; j < numbers.size(); j++) {
+                    if (numbers.get(j) > middle) {
+                        max = numbers.get(j);
+                        maxIndex = j;
+                        break;
+                    }
+                }
+            }
             numbers.set(maxIndex, min);
             numbers.set(minIndex, max);
-            System.out.println("variant 1");
+//            System.out.println("variant 1");
         }
+//        System.out.println(numbers);
 
-
-    System.out.println(numbers);
         while (maxIndex < minIndex) {
-            System.out.println("in");
+//            System.out.println("in");
             for (int j = maxIndex + 1; j < numbers.size(); j++) {
                 if (numbers.get(j) >= middle) {
                     max = numbers.get(j);
@@ -113,13 +124,29 @@ public class FastSort3_3_5 {
                 }
             }
             if (maxIndex < minIndex && max >= middle && min <= middle) {
+                if (min == max && maxIndex == middleIndex) {
+                    for (int j = minIndex - 1; j >= 0; j--) {
+                        if (numbers.get(j) < middle) {
+                            min = numbers.get(j);
+                            minIndex = j;
+                            break;
+                        }
+                    }
+                }
+                if (min == max && minIndex == middleIndex) {
+                    for (int j = maxIndex + 1; j < numbers.size(); j++) {
+                        if (numbers.get(j) > middle) {
+                            max = numbers.get(j);
+                            maxIndex = j;
+                            break;
+                        }
+                    }
+                }
                 numbers.set(maxIndex, min);
                 numbers.set(minIndex, max);
-                System.out.println("variant2");
+//                System.out.println("variant2");
             }
-
         }
-
         return numbers;
     }
 
